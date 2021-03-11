4f28c3bcaa5b5d8aaeb4fefb271e0cf0884dd67d
Aditya Karnam <akarnam37@gmail.com>
2021-03-11 16:40:46 -0600

    fix: refactor app

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,107 @@
-import React, { Component } from 'react';
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import PropTypes from 'prop-types';
 import './app.css';
-import ReactImage from './react.png';
+import {
+  Input,
+  Layout,
+  Typography,
+} from 'antd';
+import 'antd/dist/antd.css';
+import {
+  compose,
+  withHandlers,
+  withState,
+} from 'recompose';
+import parseGithubURL from 'parse-github-url';
+import ReactMarkdown from 'react-markdown';
+import CodeBlock from './Codeblock';
 
-export default class App extends Component {
-  state = { username: null };
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
+const { Title } = Typography;
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+const AppComponent = ({
+  getGithubRepoChanges,
+  onGithubRepoChanged,
+  repoDiffsData,
+  submitting,
+}) => (
+  <div>
+    <Layout className="layout">
+      <Header>
+        <Search
+          allowClear
+          onChange={onGithubRepoChanged}
+          onSearch={getGithubRepoChanges}
+          style={{ padding: '10px 50px' }}
+          placeholder="https://github.com/mui-org/material-ui"
+          enterButton="Search"
+          size="large"
+          loading={submitting}
+        />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Title level={3} style={{ padding: '20px 0' }}>Breaking Changes and Versions</Title>
+        <div className="site-layout-content">
+          {repoDiffsData}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design | Created with
+        <span role="img" aria-label="heart-emoji">
+          ❤️
+        </span>
+        by Aditya Karnam
+      </Footer>
+    </Layout>
+  </div>
+);
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
-}
+AppComponent.propTypes = {
+  getGithubRepoChanges: PropTypes.func,
+  onGithubRepoChanged: PropTypes.func,
+  repoDiffsData: PropTypes.shape([]),
+  submitting: PropTypes.bool,
+};
+
+const enhance = compose(
+  withState('repoDiffsData', 'setRepoDiffsData', []),
+  withState('submitting', 'setSubmitting', false),
+  withState('githubRepo', 'setGithubRepo', null),
+  withHandlers({
+    onGithubRepoChanged: ({ setGithubRepo, setRepoDiffsData }) => (e) => {
+      setGithubRepo(e.target.value);
+      if (e.target.value === '') setRepoDiffsData([]);
+    },
+    getGithubRepoChanges: ({ githubRepo, setRepoDiffsData, setSubmitting }) => () => {
+      if (githubRepo === '') return;
+      const parsedGithubInfo = parseGithubURL(githubRepo);
+      const { owner, name } = parsedGithubInfo;
+      if (owner && name) {
+        setSubmitting(true);
+        fetch(`/api/repo/${owner}/${name}/changes/breaking`)
+          .then(res => res.json())
+          .then((repoData) => {
+            setSubmitting(false);
+            setRepoDiffsData(repoData.data.map(diffData => (
+              <div>
+                <Title level={3}>
+                  Release:
+                  {diffData.release}
+                </Title>
+                <ReactMarkdown
+                  allowDangerousHtml
+                  renderers={{ code: CodeBlock }}
+                >
+                  {diffData.diffs[0]}
+                </ReactMarkdown>
+              </div>
+            )));
+          });
+      }
+    },
+  }),
+);
+
+export default enhance(AppComponent);
