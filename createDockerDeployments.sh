ee838508bfb01af9f5fa0eae1d6a46775759fe30
Aditya Karnam <akarnam37@gmail.com>
2021-03-18 18:01:09 -0500

    fix: script, token ignore

--- createDockerDeployments.sh
+++ createDockerDeployments.sh
@@ -2,17 +2,9 @@
 
 echo "Building client application"
 docker build -f app.Dockerfile .  -t breaking-changes-notifier-client > /dev/null &
-while kill -0 $!; do
-    printf '.' > /dev/tty
-    sleep 2
-done
 echo "Built client app."
 
 echo "Building server app"
 docker build -f server.Dockerfile .  -t breaking-changes-notifier-app > /dev/null &
-while kill -0 $!; do
-    printf '.' > /dev/tty
-    sleep 2
-done
 echo "Built Server app."
 
